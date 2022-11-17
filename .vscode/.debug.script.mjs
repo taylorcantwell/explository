b4efd51c0a587a75b807d9c2a622e2daf5b0d241
Taylor <taylor@birchal.com>
2022-11-17 12:42:22 +1100

    init

---
+++ .vscode/.debug.script.mjs
@@ -0,0 +1,21 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { createRequire } from "module";
+import { spawn } from "child_process";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const require = createRequire(import.meta.url);
+const pkg = require("../package.json");
+
+// write .debug.env
+const envContent = Object.entries(pkg.debug.env).map(
+  ([key, val]) => `${key}=${val}`
+);
+fs.writeFileSync(path.join(__dirname, ".debug.env"), envContent.join("\n"));
+
+// bootstrap
+spawn(process.platform === "win32" ? "npm.cmd" : "npm", ["run", "dev"], {
+  stdio: "inherit",
+  env: Object.assign(process.env, { VSCODE_DEBUG: "true" }),
+});
